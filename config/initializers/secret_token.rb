da40de3c67e768b80c731e3ebf1a054112049f1e
inayasu <inayasu19@gmail.com>
2016-07-12 10:36:21 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TermdicApp::Application.config.secret_key_base = '8c153b390988ad0771bcc57cca6b1b40ff84ad5e77596abec9639da1b7eb07da543a66177344665992261a3ddffd5185d1d9ef9248702526edfe2a23d9a1c14d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TermdicApp::Application.config.session_store :cookie_store, key: '_termdic_app_session'
